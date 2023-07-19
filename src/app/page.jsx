8d2f2e659a8c18f0966b61c2607275f222409849
HS-Jeong88 <wjdgksto88@gmail.com>
2023-07-20 03:40:18 +0900

    `

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,8 +6,8 @@ import { useEffect, useRef, useState } from "react";
 export default function Home() {
   const [inputValue, setInputValue] = useState([]);
   const inputRef = useRef(null);
-  console.log(inputRef.current?.files);
   useEffect(() => {
+    console.log(inputRef.current?.files);
     const newArr = [];
     for (const items of inputRef.current.files) {
       newArr.push(items);
@@ -17,19 +17,23 @@ export default function Home() {
 
   const onsubmit = async (e) => {
     e.preventDefault();
-    const formData = new FormData();
     const targetData = inputRef.current.files;
-    const jsonData = "1";
-    for (const item of targetData) {
+    if (targetData.length === 0) return alert("파일을 선택해주세요.");
+    const formData = new FormData();
+    for (const item of inputRef.current.files) {
       formData.append("files", item);
     }
-    formData.append("data", jsonData);
-    try {
-      const response = await axios.post("http://localhost:8000/api/upload/approval", formData);
-      console.log(response.data);
-    } catch (error) {
+    formData.append("data", "1");
+    const index = "1";
+    const response = await axios({
+      method: "post",
+      url: `http://localhost:8000/api/upload/approval/${index}`,
+      data: formData,
+      headers: { "Content-Type": "multipart/form-data", Accept: "application/json" },
+    }).catch((error) => {
       console.log(error);
-    }
+    });
+    console.log(response.data);
   };
 
   return (
